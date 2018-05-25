42f9074ccfaa7c23657e5be2273eae7115413154
Madhu.Gaddam <Madhu.Gaddam@X03285.amsphere.local>
2018-05-25 12:50:27 +0100

    committing

---
+++ CucumberTestProject/test-output/cucumber-htmlreport/report.js
@@ -0,0 +1,174 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Bikes.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Verifying bikes based on filter",
+  "description": "As a user, assuming I have access to a modern web browser (Chrome) with an internet connection and I am visiting the page",
+  "id": "verifying-bikes-based-on-filter",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 8650243876,
+  "status": "passed"
+});
+formatter.background({
+  "line": 4,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 5,
+  "name": "Open the browser and I am visiting the page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I would like to see a list or grid of bikes",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ScenarioOneStepDef.Open_the_browser_and_I_am_visiting_the_page()"
+});
+formatter.result({
+  "duration": 5787668328,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ScenarioOneStepDef.I_would_like_to_see_a_list_grid_of_bikes()"
+});
+formatter.result({
+  "duration": 172407343,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "I would like to see a list or grid of bikes",
+  "description": "",
+  "id": "verifying-bikes-based-on-filter;i-would-like-to-see-a-list-or-grid-of-bikes",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.after({
+  "duration": 633882823,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 3484494321,
+  "status": "passed"
+});
+formatter.background({
+  "line": 4,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 5,
+  "name": "Open the browser and I am visiting the page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I would like to see a list or grid of bikes",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ScenarioOneStepDef.Open_the_browser_and_I_am_visiting_the_page()"
+});
+formatter.result({
+  "duration": 5803967334,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ScenarioOneStepDef.I_would_like_to_see_a_list_grid_of_bikes()"
+});
+formatter.result({
+  "duration": 129040987,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "I would like to see a name, an image, a description and the class for each bike",
+  "description": "",
+  "id": "verifying-bikes-based-on-filter;i-would-like-to-see-a-name,-an-image,-a-description-and-the-class-for-each-bike",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 12,
+  "name": "I would like to see the name an image a description and the class name for each  bike",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ScenarioOneStepDef.I_would_like_to_see_the_name_an_image_a_description_and_the_class_name_for_each_bike()"
+});
+formatter.result({
+  "duration": 484418278,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 626380625,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 4489208563,
+  "status": "passed"
+});
+formatter.background({
+  "line": 4,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 5,
+  "name": "Open the browser and I am visiting the page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I would like to see a list or grid of bikes",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ScenarioOneStepDef.Open_the_browser_and_I_am_visiting_the_page()"
+});
+formatter.result({
+  "duration": 4139796268,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ScenarioOneStepDef.I_would_like_to_see_a_list_grid_of_bikes()"
+});
+formatter.result({
+  "duration": 134890751,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "I would like to be able to sort the bikes into a custom order based on class",
+  "description": "",
+  "id": "verifying-bikes-based-on-filter;i-would-like-to-be-able-to-sort-the-bikes-into-a-custom-order-based-on-class",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 16,
+  "name": "I filter by the class bikes should be able to sort into custom order",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "ScenarioOneStepDef.I_would_like_to_be_able_to_sort_the_bikes_into_a_custom_order_based_onclass()"
+});
+formatter.result({
+  "duration": 6911195915,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 603228357,
+  "status": "passed"
+});
+});
